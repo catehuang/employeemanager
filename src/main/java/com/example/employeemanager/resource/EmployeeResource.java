ae98a21b5b3125371151028e018d90a95f27cf6d
Cate Huang <s0350228@gmail.com>
2022-06-02 19:10:59 -0600

    add @CrossOrigin for CORS

--- src/main/java/com/example/employeemanager/resource/EmployeeResource.java
+++ src/main/java/com/example/employeemanager/resource/EmployeeResource.java
@@ -8,49 +8,49 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// @RestController annotation in order to simplify the creation of RESTful web services.
+// Cross-origin resource sharing is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.
+// @RequestMapping annotation is used to map web requests onto specific handler classes and/or handler methods
+@RestController
+@CrossOrigin
+@RequestMapping("/employee")
 public class EmployeeResource {
-    // @RestController annotation in order to simplify the creation of RESTful web services.
-    // @RequestMapping annotation is used to map web requests onto specific handler classes and/or handler methods
-    @RestController
-    @RequestMapping("/employee")
-    public class EmployeeResource {
-        private final EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
-        public EmployeeResource(EmployeeService employeeService) {
-            this.employeeService = employeeService;
-        }
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
-        @GetMapping("/all")
-        // Generic method
-        public ResponseEntity<List<Employee>> getAllEmployees() {
-            List<Employee> employees = employeeService.findAllEmployees();
-            return new ResponseEntity<>(employees, HttpStatus.OK);
-        }
+    @GetMapping("/all")
+    // Generic method
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
 
-        // the @PathVariable annotation to extract the templated part of the URI,
-        @GetMapping("/find/{id}")
-        public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
-            Employee employee = employeeService.findEmployeeById(id);
-            return new ResponseEntity<>(employee, HttpStatus.OK);
-        }
+    // the @PathVariable annotation to extract the templated part of the URI,
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
 
-        // @RequestBody annotations is used to bind the HTTP request body with a domain object in method parameter or return type.
-        @PostMapping("/add")
-        public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
-            Employee newEmployee = employeeService.addEmployee(employee);
-            return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
-        }
+    // @RequestBody annotations is used to bind the HTTP request body with a domain object in method parameter or return type.
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
+    }
 
-        @PutMapping("/update")
-        public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
-            Employee updateEmployee = employeeService.updateEmployee(employee);
-            return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
-        }
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
+    }
 
-        @DeleteMapping("/delete/{id}")
-        public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
-            employeeService.deleteEmployee(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
