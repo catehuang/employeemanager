47d43487766188105c4716f2c18d420bd473ce76
Cate Huang <s0350228@gmail.com>
2022-06-04 12:53:10 -0600

    change UI layout

--- src/main/frontend/src/components/EditEmployee.js
+++ src/main/frontend/src/components/EditEmployee.js
@@ -19,15 +19,15 @@ function EditEmployee() {
                 const getEmployee = async () => {
                         try {
                                 // id can only be in the same string and uses dollar sign and curly braces or it will be passed as an object
-                                const employee = await axios.get(`/find/${id}`, {                       
-                        });
-                        
-                        setName(employee.data.name); 
-                        setJobTitle(employee.data.jobTitle); 
-                        setEmail(employee.data.email); 
-                        setPhone(employee.data.phone); 
-                        setImageUrl(employee.data.imageUrl); 
-                        setEmployeeCode(employee.data.employeeCode); 
+                                const employee = await axios.get(`/find/${id}`, {
+                                });
+
+                                setName(employee.data.name);
+                                setJobTitle(employee.data.jobTitle);
+                                setEmail(employee.data.email);
+                                setPhone(employee.data.phone);
+                                setImageUrl(employee.data.imageUrl);
+                                setEmployeeCode(employee.data.employeeCode);
 
                         } catch (err) {
                                 console.log(`Error: ${err.message}`);
@@ -57,27 +57,41 @@ function EditEmployee() {
         return (
                 <div class="bg-yellow-50 h-screen">
                         <Header />
-                        <div class="w-96 h-120 mx-auto">
+                        <div class="w-1/4 h-120 mx-auto">
                                 <h2 class="text-center text-xl font-bold my-10">Edit an Employee</h2>
-                                <form class="border border-gray-600 rounded-lg p-5" onSubmit={updateEmployee} >
-                                        <div class="px-5">
-                                                <label htmlFor="newEmployeeName" class="block mb-1">Name</label>
-                                                <input id="newEmployeeName" type="text" value={name} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
-                                                        e => setName(e.target.value)
-                                                } />
-                                        </div>
+                                <form class="min-w-96 border border-gray-600 rounded-lg p-5" onSubmit={updateEmployee} >
+                                        <div class="grid grid-cols-2">
+                                                <div class="px-5">
+                                                        <img src={imageUrl} alt="" class="border border-gray-300 rounded-lg mx-auto max-h-56" />
+                                                </div>
+                                                <div>
+                                                        <div class="px-5">
+                                                                <label htmlFor="newEmployeeEmployeeCode" class="block mb-1">Employee Code</label>
+                                                                <input id="newEmployeeEmployeeCode" type="text" value={employeeCode} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
+                                                                        e => setEmployeeCode(e.target.value)
+                                                                } />
+                                                        </div>
 
-                                        <div class="px-5">
-                                                <label htmlFor="newEmployeeJobTitle" class="block mb-1">Job Title</label>
-                                                <input id="newEmployeeJobTitle" type="text" value={jobTitle} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
-                                                        e => setJobTitle(e.target.value)
-                                                } />
-                                        </div>
+                                                        <div class="px-5">
+                                                                <label htmlFor="newEmployeeName" class="block mb-1">Name</label>
+                                                                <input id="newEmployeeName" type="text" value={name} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
+                                                                        e => setName(e.target.value)
+                                                                } />
+                                                        </div>
 
+                                                        <div class="px-5">
+                                                                <label htmlFor="newEmployeeJobTitle" class="block mb-1">Job Title</label>
+                                                                <input id="newEmployeeJobTitle" type="text" value={jobTitle} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
+                                                                        e => setJobTitle(e.target.value)
+                                                                } />
+                                                        </div>
+                                                </div>
+                                        </div>
+                                        
                                         <div class="px-5">
-                                                <label htmlFor="newEmployeeEmployeeCode" class="block mb-1">Employee Code</label>
-                                                <input id="newEmployeeEmployeeCode" type="text" value={employeeCode} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
-                                                        e => setEmployeeCode(e.target.value)
+                                                <label htmlFor="newEmployeeImageURL" class="block mb-1">Image URL</label>
+                                                <input id="newEmployeeImageURL" type="text" value={imageUrl} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
+                                                        e => setImageUrl(e.target.value)
                                                 } />
                                         </div>
 
@@ -95,12 +109,7 @@ function EditEmployee() {
                                                 } />
                                         </div>
 
-                                        <div class="px-5">
-                                                <label htmlFor="newEmployeeImageURL" class="block mb-1">Image URL</label>
-                                                <input id="newEmployeeImageURL" type="text" value={imageUrl} class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
-                                                        e => setImageUrl(e.target.value)
-                                                } />
-                                        </div>
+
 
                                         <div class="p-5">
                                                 <button type="submit" class="w-full bg-amber-300 p-3 rounded-lg">Submit</button>
--- src/main/frontend/src/components/NewEmployee.js
+++ src/main/frontend/src/components/NewEmployee.js
@@ -32,27 +32,40 @@ function NewEmployee() {
         return (
                 <div class="bg-yellow-50 h-screen">
                         <Header />
-                        <div class="w-96 h-120 mx-auto">
+                        <div class="w-1/4 h-120 mx-auto">
                                 <h2 class="text-center text-xl font-bold my-10">Add a New Employee</h2>
-                                <form class="border border-gray-600 rounded-lg p-5" onSubmit={addEmployee} >
-                                        <div class="px-5">
-                                                <label htmlFor="newEmployeeName" class="block mb-1">Name</label>
-                                                <input id="newEmployeeName" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2"  required onChange={
-                                                        e => setName(e.target.value)
-                                                } />
+                                <form class="min-w-96  border border-gray-600 rounded-lg p-5" onSubmit={addEmployee} >
+                                        <div class="grid grid-cols-2">
+                                                <div class="px-5">
+                                                        <img src={imageUrl} alt="" class="border border-gray-300 rounded-lg mx-auto max-h-56" />
+                                                </div>
+
+                                                <div>
+                                                        <div class="px-5">
+                                                                <label htmlFor="newEmployeeEmployeeCode" class="block mb-1">Employee Code</label>
+                                                                <input id="newEmployeeEmployeeCode" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
+                                                                        e => setEmployeeCode(e.target.value)
+                                                                } />
+                                                        </div>
+                                                        <div class="px-5">
+                                                                <label htmlFor="newEmployeeName" class="block mb-1">Name</label>
+                                                                <input id="newEmployeeName" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
+                                                                        e => setName(e.target.value)
+                                                                } />
+                                                        </div>
+                                                        <div class="px-5">
+                                                                <label htmlFor="newEmployeeJobTitle" class="block mb-1">Job Title</label>
+                                                                <input id="newEmployeeJobTitle" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
+                                                                        e => setJobTitle(e.target.value)
+                                                                } />
+                                                        </div>
+                                                </div>
                                         </div>
 
                                         <div class="px-5">
-                                                <label htmlFor="newEmployeeJobTitle" class="block mb-1">Job Title</label>
-                                                <input id="newEmployeeJobTitle" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2"  onChange={
-                                                        e => setJobTitle(e.target.value)
-                                                } />
-                                        </div>
-
-                                        <div class="px-5">
-                                                <label htmlFor="newEmployeeEmployeeCode" class="block mb-1">Employee Code</label>
-                                                <input id="newEmployeeEmployeeCode" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" required onChange={
-                                                        e => setEmployeeCode(e.target.value)
+                                                <label htmlFor="newEmployeeImageURL" class="block mb-1">Image URL</label>
+                                                <input id="newEmployeeImageURL" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
+                                                        e => setImageUrl(e.target.value)
                                                 } />
                                         </div>
 
@@ -69,13 +82,8 @@ function NewEmployee() {
                                                         e => setPhone(e.target.value)
                                                 } />
                                         </div>
-                                        
-                                        <div class="px-5">
-                                                <label htmlFor="newEmployeeImageURL" class="block mb-1">Image URL</label>
-                                                <input id="newEmployeeImageURL" type="text" class="border border-gray-300 w-full rounded-l mb-5 py-1 px-2" onChange={
-                                                        e => setImageUrl(e.target.value)
-                                                } />
-                                        </div>
+
+
 
                                         <div class="p-5">
                                                 <button type="submit" class="w-full bg-amber-300 p-3 rounded-lg">Submit</button>
